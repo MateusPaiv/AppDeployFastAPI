9b1ea4e5c1f9f8b2eda88d821d0649f6dfaddd52
MateusPaiv <mateuspaiva0118@gmail.com>
2024-12-02 14:51:56 -0300

    feat: add env database prodution and devlopment

--- database.py
+++ database.py
@@ -1,9 +1,12 @@
 from sqlmodel import SQLModel, create_engine
+import os
 
-DATABASE_URL = "postgresql://postgres.gukfcizmqwjthzueiyqm:QhXXjU3OAeDEDgxQ@aws-0-sa-east-1.pooler.supabase.com:6543/postgres"
-
+DATABASE_URL = os.getenv(
+    "DB_URL_PROD" if os.getenv("ENVIRONMENT", "production") != "development" else "DB_URL_DEV"
+)
 try:
     engine = create_engine(DATABASE_URL, echo=True)
     SQLModel.metadata.create_all(engine)
+        
 except Exception as e:
     print(f"Erro ao conectar ao banco de dados: {e}")
