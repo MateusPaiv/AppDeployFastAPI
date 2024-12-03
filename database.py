d4b47a08da5522186302e0ed65886f2a9d294203
MateusPaiv <mateuspaiva0118@gmail.com>
2024-12-03 11:01:15 -0300

    feat: add only env

--- database.py
+++ database.py
@@ -1,6 +1,9 @@
 from sqlmodel import SQLModel, create_engine
+import os
 
-DATABASE_URL = "postgresql://postgres.gukfcizmqwjthzueiyqm:QhXXjU3OAeDEDgxQ@aws-0-sa-east-1.pooler.supabase.com:6543/postgres"
+environment = os.getenv("ENVIRONMENT", "development")
+
+DATABASE_URL = "postgresql://postgres.gukfcizmqwjthzueiyqm:QhXXjU3OAeDEDgxQ@aws-0-sa-east-1.pooler.supabase.com:6543/postgres" if environment == "development" else "postgresql://postgres.qylcbshcbfcfnvvtsmmo:lumy3099@aws-0-sa-east-1.pooler.supabase.com:6543/postgres"
 
 try:
     engine = create_engine(DATABASE_URL, echo=True)
--- main.py
+++ main.py
@@ -5,11 +5,25 @@ from database import engine
 from models import Produto, Sale
 import uvicorn
 import os
+from fastapi.middleware.cors import CORSMiddleware
 
-
-environment = os.getenv("ENVIRONMENT", "production")
+environment = os.getenv("ENVIRONMENT", "development")
 app = FastAPI()
 admin = Admin(app, engine)
+
+origins = [
+    "http://localhost",
+    "http://localhost:62081",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 class Produto(ModelView, model=Produto):
     column_list = [Produto.id, Produto.nome]
@@ -29,10 +43,11 @@ def list_produtos():
 
 @app.get("/")
 def home():
-    if environment == "development":
-        return {"message": "Running in development mode"}
-    else:
+    if environment == "production":
         return {"message": "Running in production mode"}
+        
+    return {"message": "Running in development mode"}
+    
     
 admin.add_view(Produto)
 admin.add_view(Sale)
